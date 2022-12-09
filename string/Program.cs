66c02eed9708bc8ff3fdf7f2b3bbc26f989e985c
Elena <kallaur@bk.ru>
2022-12-09 16:55:02 -0500

    fillArray

--- string/Program.cs
+++ string/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//https://gbcdn.mrgcdn.ru/uploads/asset/4312773/attachment/ed8c1f2c15da325114976e1c313ef5f8.png
+// написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше или равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо 
+// задать на старте выполнения алгоритма. При решении не рекоментуется пользоваться коллекциями, лучше
+// обойтись исключительно массивами.
+//  ["hello", "2","world", ":-)"] -> ["2", ":-)"]
+
+Console.WriteLine("введите значения массива string, не более 4 строк");
+String [] random_in = new string[4];
+for(int i = 0; i < random_in.Length; i++)
+{
+   random_in[i] = Console.ReadLine();
+}
+
+
+
